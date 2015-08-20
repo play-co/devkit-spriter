6262f1edac39d8b2c82fdcfe7a770210ff26de94
Martin Hunt <martin@gameclosure.com>
2015-08-19 17:45:06 -0700

    DiskCache: check for target spritesheets
    If a user deletes a spritesheet in the output folder, resprite the
    source group regardless of cache.

--- DiskCache.js
+++ DiskCache.js
@@ -1,16 +1,27 @@
 var Promise = require('bluebird');
-var fs = Promise.promisifyAll(require('graceful-fs'));
+var path = require('path');
+var fs = require('graceful-fs');
+
+var exists = function (filename) {
+  return new Promise(function (resolve, reject) {
+    fs.exists(filename, function (exists) {
+      if (exists) {
+        resolve();
+      } else {
+        reject();
+      }
+    });
+  });
+};
+
+var readFile = Promise.promisify(fs.readFile);
+var writeFile = Promise.promisify(fs.writeFile);
+var stat = Promise.promisify(fs.stat);
 
 module.exports = DiskCache;
-
-function DiskCache(filename, data, outputDirectory) {
-  this._filename = filename;
-  this._data = data;
-  this._outputDirectory = outputDirectory;
-}
-
-DiskCache.load = function (filename, outputDirectory) {
-  return fs.readFileAsync(filename, 'utf-8')
+module.exports.NotCachedError = NotCachedError;
+module.exports.load = function (filename, outputDirectory) {
+  return readFile(filename, 'utf-8')
     .then(function (contents) {
       return JSON.parse(contents);
     })
@@ -21,6 +32,16 @@ DiskCache.load = function (filename, outputDirectory) {
       return new DiskCache(filename, data, outputDirectory);
     });
 };
+
+function NotCachedError() { this.message = 'not cached'; }
+NotCachedError.prototype = Object.create(Error.prototype);
+NotCachedError.prototype.constructor = NotCachedError;
+
+function DiskCache(filename, data, outputDirectory) {
+  this._filename = filename;
+  this._data = data;
+  this._outputDirectory = outputDirectory;
+}
 
 DiskCache.prototype.get = function (key, filenames) {
   var cache = this._data[key];
@@ -33,9 +54,10 @@ DiskCache.prototype.get = function (key, filenames) {
 
   return Promise
     .resolve(filenames)
+    .bind(this)
     .map(function (filename) {
       var cachedMtime = cache.mtime[filename] || 0;
-      return fs.statAsync(filename)
+      return stat(filename)
         .then(function (stat) {
           var mtime = stat.mtime.getTime();
           if (mtime > cachedMtime) {
@@ -46,10 +68,18 @@ DiskCache.prototype.get = function (key, filenames) {
     })
     .then(function () {
       if (isCached) {
-        return cache.value;
+        return Object.keys(cache.value);
       } else {
-        throw new Error("not cached");
+        throw new NotCachedError();
       }
+    })
+    .map(function (sheetname) {
+      // for the destination files, just check that each file exists
+      var filename = path.join(this._outputDirectory, sheetname);
+      return exists(filename);
+    })
+    .catch(function () {
+      throw new NotCachedError();
     });
 };
 
@@ -59,5 +89,5 @@ DiskCache.prototype.set = function (key, value) {
 };
 
 DiskCache.prototype.save = function () {
-  return fs.writeFileAsync(this._filename, JSON.stringify(this._data));
+  return writeFile(this._filename, JSON.stringify(this._data));
 };
