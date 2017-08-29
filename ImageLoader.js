f69d549275aec44e25daf000f47e77e41825eaf1
Ivan Reshetnikov <keenn2007@gmail.com>
2017-08-29 18:44:40 +0200

    added load concurrency limit

--- ImageLoader.js
+++ ImageLoader.js
@@ -1,10 +1,21 @@
 var ImageBuffer = require('./ImageBuffer');
 var ImageInfo = require('./ImageInfo');
 var Promise = require('bluebird');
+var Queue = require('promise-queue');
+
+Queue.configure(Promise);
+
+var maxConcurrent = (
+  process.env.DEVKIT_SPRITER_LOADING_CONCURRENCY
+    ? parseInt(process.env.DEVKIT_SPRITER_LOADING_CONCURRENCY)
+    : 200
+);
+
+var queue = new Queue(maxConcurrent, Infinity);
 
 module.exports = ImageLoader;
 
-function ImageLoader() {}
+function ImageLoader () {}
 
 ImageLoader.prototype.load = function (images, scale) {
   var errors = {};
@@ -27,15 +38,17 @@ ImageLoader.prototype.load = function (images, scale) {
 };
 
 ImageLoader.prototype.get = function (filename, scale) {
-  return new ImageBuffer({filename: filename})
-    .load()
-    .then(function (buffer) {
-      if (scale && scale !== 1) {
-        var width = Math.ceil(buffer.bitmap.width * scale);
-        var height = Math.ceil(buffer.bitmap.height * scale);
-        buffer.resize(width, height);
-      }
+  return queue.add(function () {
+    return new ImageBuffer({filename: filename})
+      .load()
+      .then(function (buffer) {
+        if (scale && scale !== 1) {
+          var width = Math.ceil(buffer.bitmap.width * scale);
+          var height = Math.ceil(buffer.bitmap.height * scale);
+          buffer.resize(width, height);
+        }
 
-      return new ImageInfo(filename, buffer, scale);
-    });
+        return new ImageInfo(filename, buffer, scale);
+      });
+  });
 };
